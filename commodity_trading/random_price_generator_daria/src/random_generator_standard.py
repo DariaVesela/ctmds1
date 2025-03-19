6c327687ac0af893b9a4f90a0630da3e9ab2717f
Daria Vesela <daria1vesela@gmail.com>
2025-03-19 19:45:42 +0000

    added validation for standard generation

--- commodity_trading/random_price_generator_daria/src/random_generator_standard.py
+++ commodity_trading/random_price_generator_daria/src/random_generator_standard.py
@@ -3,7 +3,10 @@ import random
 
 class standardPrices(PriceGeneratorBase):
     def _generate_prices_implementation(self, num: int) -> list[float]:
-        return [round(random.uniform(0, 100), 2) for _ in range(num)]
+        if not isinstance(num, int) or num < 0:
+            raise ValueError("num must be a positive integer")
+        else:
+            return [round(random.uniform(0, 100), 2) for _ in range(num)]
     
     def standard_price_generation(self, num:int)-> list[float]:
         return self._generate_prices_implementation(num)
